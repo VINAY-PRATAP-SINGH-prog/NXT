f7ec5a4afa48d089c9c26d252261234f29a07907
VINAY-PRATAP-SINGH-prog <240340@kit.ac.in>
2026-06-29 09:00:55 +0000

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,24 @@
 import monitor
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 from events import events
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="../frontend")
 CORS(app)
 
 
 
 @app.route("/")
+def index():
+    return send_from_directory(app.static_folder,"index.html")
 
-def home():
-    return "NXT Backend Running"
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
 
 @app.route("/events")
 def get_events():
     return jsonify(events)
 if __name__ == "__main__":
     monitor.start_monitor()
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
